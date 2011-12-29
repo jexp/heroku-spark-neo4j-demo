e875356a52eee20a0ad6222f33b99735c9ddaa23
Michael Hunger <github@jexp.de>
2011-12-29 04:40:48 +0100

    added port

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,12 @@ import spark.Response;
 import spark.Route;
 
 import static spark.Spark.get;
+import static spark.Spark.setPort;
 
 public class Main {
 
     public static void main(String[] args) {
+        setPort(Integer.parseInt(System.getenv("PORT")));
         get(new Route("/hello") {
             public Object handle(Request request, Response response) {
                 return "Hello World!";
