3f368d2643ba2e52753b5a45a515534482303ee4
Michael Hunger <github@jexp.de>
2011-12-29 05:06:34 +0100

    added dependency to neo4j, and /neo endpoint

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,25 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
+import static java.lang.System.getenv;
 import static spark.Spark.get;
 import static spark.Spark.setPort;
+import org.neo4j.rest.graphdb.RestGraphDatabase;
+import org.neo4j.graphdb.GraphDatabaseService;
 
 public class Main {
 
     public static void main(String[] args) {
-        setPort(Integer.parseInt(System.getenv("PORT")));
+        setPort(Integer.parseInt(getenv("PORT")));
         get(new Route("/hello") {
             public Object handle(Request request, Response response) {
                 return "Hello World!";
+            }
+        });
+        final GraphDatabaseService gdb = new RestGraphDatabase(getenv("NEO4J_REST_URL"), getenv("NEO4J_LOGIN"), getenv("NEO4J_PASSWORD"));
+        get(new Route("/neo") {
+            public Object handle(Request request, Response response) {
+                return "Hello World from the Neo4j-Node "+gdb.getReferenceNode();
             }
         });
     }
